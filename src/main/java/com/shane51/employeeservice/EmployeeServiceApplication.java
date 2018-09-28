392a339aaaf3c9f360e0313d3f2cc83206518302
Wuyi Qin <wyqin@thoughtworks.com>
2018-09-28 09:49:06 +0800

    [Tech] add api docs for employee service

--- src/main/java/com/shane51/employeeservice/EmployeeServiceApplication.java
+++ src/main/java/com/shane51/employeeservice/EmployeeServiceApplication.java
@@ -6,6 +6,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
@@ -15,6 +20,17 @@ public class EmployeeServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EmployeeServiceApplication.class, args);
+    }
+
+
+    @Bean
+    public Docket swaggerApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.shane51.employeeservice.controller"))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(new ApiInfoBuilder().version("1.0").title("Empoylee API").description("Documentation Employee V1.0").build());
     }
 
     @Bean
