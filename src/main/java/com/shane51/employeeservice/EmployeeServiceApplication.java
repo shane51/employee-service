eedc65c7b9f5e9ef46d50ecc14976a7e043cc69a
Wuyi Qin <wyqin@thoughtworks.com>
2018-09-28 09:33:14 +0800

    [Tech] add api for employee service

--- src/main/java/com/shane51/employeeservice/EmployeeServiceApplication.java
+++ src/main/java/com/shane51/employeeservice/EmployeeServiceApplication.java
@@ -1,14 +1,35 @@
 package com.shane51.employeeservice;
 
+import com.shane51.employeeservice.model.Employee;
+import com.shane51.employeeservice.repository.EmployeeRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableSwagger2
 public class EmployeeServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EmployeeServiceApplication.class, args);
+    }
+
+    @Bean
+    EmployeeRepository repository() {
+        EmployeeRepository repository = new EmployeeRepository();
+        repository.add(new Employee(1L, 1L, "John Smith", 34, "Analyst"));
+        repository.add(new Employee(1L, 1L, "Darren Hamilton", 37, "Manager"));
+        repository.add(new Employee(1L, 1L, "Tom Scott", 26, "Developer"));
+        repository.add(new Employee(1L, 2L, "Anna London", 39, "Analyst"));
+        repository.add(new Employee(1L, 2L, "Patrick Dempsey", 27, "Developer"));
+        repository.add(new Employee(2L, 3L, "Kevin Price", 38, "Developer"));
+        repository.add(new Employee(2L, 3L, "Ian Scott", 34, "Developer"));
+        repository.add(new Employee(2L, 3L, "Andrew Campton", 30, "Manager"));
+        repository.add(new Employee(2L, 4L, "Steve Franklin", 25, "Developer"));
+        repository.add(new Employee(2L, 4L, "Elisabeth Smith", 30, "Developer"));
+        return repository;
     }
 }
